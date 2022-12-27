ece1a317470b67c2c1f1587c46ac718f2c715b4a
MBVD <vasiliidiu3102@gmail.com>
2022-12-27 23:36:06 +0600

    new 2 cards

--- app/IrbisPage/models.py
+++ app/IrbisPage/models.py
@@ -6,6 +6,7 @@ class Card(models.Model):
     pretitle = tinymce_models.HTMLField(verbose_name = 'Перед Карточкой', blank=True)
     title = models.CharField(verbose_name = 'Оглавление', max_length=50)
     content = tinymce_models.HTMLField(verbose_name = 'Контент', blank=True)
+    consists_of_images = models.BooleanField(default=False)
     
     class Meta:
         verbose_name = "Карточка"
