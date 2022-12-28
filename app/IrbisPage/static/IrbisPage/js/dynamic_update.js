29e0194bfed4b7cd5757f6e7f4fffa7eb67922c0
MBVD <vasiliidiu3102@gmail.com>
2022-12-29 00:22:13 +0600

    header for mobilr

---
+++ app/IrbisPage/static/IrbisPage/js/dynamic_update.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting){
+            entry.target.classList.add("show")
+            entry.target.classList.remove("hidden")
+        }
+    })
+})
+$('.hidden').each(function(id, el){
+    observer.observe(el)
+})  
